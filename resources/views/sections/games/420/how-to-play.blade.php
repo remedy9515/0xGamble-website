642a537636460f783d0c9a58680b8f77687a71ed
Remedy <remedy9515@gmail.com>
2023-07-16 23:07:42 +0300

    Update 420 how-to-play

--- resources/views/sections/games/420/how-to-play.blade.php
+++ resources/views/sections/games/420/how-to-play.blade.php
@@ -5,15 +5,15 @@
             <div class="number-circle">
                 <div class="number">1</div>
             </div>
-            <div class="hot-to-step">This is the first step and you should do it. I will repeat this text so it will be longer.</div>
+            <div class="hot-to-step">Choose the maximum number of 1UCK to use for the round.</div>
             <div class="number-circle">
                 <div class="number">2</div>
             </div>
-            <div class="hot-to-step">This is the first step and you should do it. I will repeat this text so it will be longer.</div>
+            <div class="hot-to-step">Generate your 1UCKY NUMBER for the round. This is a random number between 0 and the maximum number you chose in step one.</div>
             <div class="number-circle">
                 <div class="number">3</div>
             </div>
-            <div class="hot-to-step">This is the first step and you should do it. I will repeat this text so it will be longer.</div>
+            <div class="hot-to-step">Play your 1UCKY NUMBER by sending it as the amount of 1UCK in a transaction to yourself.</div>
         </div>
     </div>
 </section>
